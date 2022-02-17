03146a737b60231342dea0352a84308498abf726
Kapil Budhwani <99356059+Kapilbudhwani@users.noreply.github.com>
2022-02-18 01:35:55 +0530

    Add files via upload

---
+++ MyFirst Notebook.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## My Jupyter Notebook on IBM Watson Studio
+
+# **Kapil Budhwani - Manager Analytics**
+
+# *I am interested in Python becacuse the flexibility it will give to play with data can't give any other programming languange. In addtion that syntax of this languange compartively simple and I wanted to excel in this langague to give different dimension to business world*
+
+# ## This progam will ask user to input any integer and display that number is odd or even
+
+# In[16]:
+
+
+n= int(input('Please enter an integer number: '))
+if (n%2)==0:
+    print('Number is even')
+else:
+    print('Number is odd')
+
+
+# In[ ]:
+
+
+
+
